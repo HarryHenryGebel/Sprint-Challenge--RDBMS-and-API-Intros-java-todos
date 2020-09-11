c2e5cfbcc19d260d3b9934f867b02428740d8176
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-11 19:11:42 -0400

    Give proper name UserServiceImpl → UserServiceImplementation

--- todos/src/main/java/com/lambdaschool/todos/services/UserServiceImplementation.java
+++ todos/src/main/java/com/lambdaschool/todos/services/UserServiceImplementation.java
@@ -3,15 +3,15 @@ package com.lambdaschool.todos.services;
 import com.lambdaschool.todos.models.User;
 import com.lambdaschool.todos.repository.UserRepository;
 import com.lambdaschool.todos.views.UserNameCountTodos;
-import java.util.ArrayList;
-import java.util.List;
-import javax.persistence.EntityNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Implements the Userservice Interface
+ * Implements the UserService Interface
  */
 @Transactional
 @Service(value = "userService")
@@ -19,20 +19,14 @@ public class UserServiceImpl implements UserService {
   /**
    * Connects this service to the User table.
    */
-  private final UserRepository userrepos;
+  private final UserRepository userRepository;
 
-  /**
-   * Connects this service to the auditing service in order to get current user name
-   */
-  private final UserAuditing userAuditing;
-
-  public UserServiceImpl(UserRepository userrepos, UserAuditing userAuditing) {
-    this.userrepos = userrepos;
-    this.userAuditing = userAuditing;
+  public UserServiceImpl(UserRepository userRepository) {
+    this.userRepository = userRepository;
   }
 
   public User findUserById(long id) throws EntityNotFoundException {
-    return userrepos
+    return userRepository
       .findById(id)
       .orElseThrow(
         () -> new EntityNotFoundException("User id " + id + " not found!")
@@ -46,19 +40,19 @@ public class UserServiceImpl implements UserService {
      * findAll returns an iterator set.
      * iterate over the iterator set and add each element to an array list.
      */
-    userrepos.findAll().iterator().forEachRemaining(list::add);
+    userRepository.findAll().iterator().forEachRemaining(list::add);
     return list;
   }
 
   @Transactional
   @Override
   public void delete(long id) {
-    userrepos
+    userRepository
       .findById(id)
       .orElseThrow(
         () -> new EntityNotFoundException("User id " + id + " not found!")
       );
-    userrepos.deleteById(id);
+    userRepository.deleteById(id);
   }
 
   @Transactional
@@ -68,7 +62,7 @@ public class UserServiceImpl implements UserService {
     user.setPassword(user.getPassword());
     user.setPrimaryEmail(user.getPrimaryEmail().toLowerCase());
 
-    return userrepos.save(user);
+    return userRepository.save(user);
   }
 
   @Override
