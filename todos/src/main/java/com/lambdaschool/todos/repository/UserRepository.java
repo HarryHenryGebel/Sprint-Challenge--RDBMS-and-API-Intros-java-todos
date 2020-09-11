8081969b26580a2ba3d77912c7d40189118c4e31
Harry Henry Gebel <hhgebel@gmail.com>
2020-09-11 18:58:20 -0400

    Eliminate all warnings in repository package

--- todos/src/main/java/com/lambdaschool/todos/repository/UserRepository.java
+++ todos/src/main/java/com/lambdaschool/todos/repository/UserRepository.java
@@ -1,9 +1,6 @@
 package com.lambdaschool.todos.repository;
 
 import com.lambdaschool.todos.models.User;
-import com.lambdaschool.todos.views.UserNameCountTodos;
-import java.util.List;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 
@@ -11,15 +8,4 @@ import org.springframework.stereotype.Component;
  * The CRUD repository connecting User to the rest of the application
  */
 @Component
-public interface UserRepository extends CrudRepository<User, Long> {
-  /**
-   * Custom query to gather the number of current todos users have
-   *
-   * @return A list of UserNameCountTodos which includes the username and count of non-completed todos
-   */
-  @Query(
-    value = "SELECT u.username as usernamerpt, count(t.todoid) as counttodos FROM users u JOIN todos t ON u.userid = t.userid WHERE NOT t.completed GROUP BY u.username ORDER BY u.username",
-    nativeQuery = true
-  )
-  List<UserNameCountTodos> getCountUserTodos();
-}
+public interface UserRepository extends CrudRepository<User, Long> {}
